fdc6e69efa7dc7379b607275765edcdb103b40cf
Tim Reichen <tim.reichen@gmail.com>
2020-10-14 09:51:59 +0200

    add example

--- cli.ts
+++ cli.ts
@@ -14,8 +14,10 @@ import { typescriptInjectSpecifiers } from "./plugins/transformers/typescript_in
 import { terser } from "./plugins/transformers/terser.ts";
 import type { CompilerOptions } from "./typescript.ts";
 import type { FileMap, Graph } from "./graph.ts";
-import { jsonLoader } from "./plugins/loaders/json.ts";
-import { json } from "./plugins/transformers/json.ts";
+import { jsonLoader } from "./plugins/loaders/json.ts"
+import { json } from "./plugins/transformers/json.ts"
+import { imageLoader } from "./plugins/loaders/image.ts"
+import { text } from "./plugins/transformers/text.ts"
 
 interface Meta {
   options: {
@@ -43,6 +45,7 @@ const loaders = [
     use: postCSSPlugins,
   }),
   jsonLoader(),
+  imageLoader()
 ];
 
 async function runBundle(
@@ -88,6 +91,9 @@ async function runBundle(
         ...compilerOptions,
         module: "system",
       },
+    }),
+    text({
+      test: (input: string) => /\.(png|svg)$/.test(input)
     }),
   ];
 
